e88a598739ca8ecad752e32c70a55cfa241351c8
paulinaOM <17030622@itcelaya.edu.mx>
2022-02-13 15:07:01 -0600

    Katas curso introduccion Python

---
+++ kata01.py
@@ -0,0 +1,9 @@
+from datetime import date
+
+print("Today's date is: "+ str(date.today()))
+
+parsec = 11
+
+lightyears = parsec * 3.26156
+
+print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
